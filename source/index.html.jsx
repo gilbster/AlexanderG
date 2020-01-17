73e15dad653b9b52fc8f48782e402b98d6cfba83
gilbster <alexanderg@vivaldi.net>
2020-01-17 16:33:49 -0600

    Update index.html.jsx

--- source/index.html.jsx
+++ source/index.html.jsx
@@ -1,10 +1,11 @@
 import About from './pages/about.html.mdx';
 import Logo from "./logo.svg";
+import Style from "./style.css";
 
 export default () => {
   return (
-    <div style="text-align:center;">
-      <Logo/>
+    <div>
+      <Logo />
       <h1>Welcome to my Portfolio Site, powered by Charge.js!</h1>
       <About />
     </div>
