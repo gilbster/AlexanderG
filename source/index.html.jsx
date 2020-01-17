c1e6046e929c01598e823434fd4b272d6c8177a4
gilbster <alexanderg@vivaldi.net>
2020-01-17 16:24:38 -0600

    Update index.html.jsx

--- source/index.html.jsx
+++ source/index.html.jsx
@@ -3,7 +3,7 @@ import About from './pages/about.html.mdx';
 export default () => {
   return (
     <>
-      <h1>Welcome to my new Charge site!</h1>
+      <h1>Welcome to my Portfolio Site, powered by Charge.js!</h1>
       <About />
     </>
   );
